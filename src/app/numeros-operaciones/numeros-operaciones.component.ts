3d16b84f1d7a1748ca43c31682d8c0449aa8968e
Miister95 <cairne_abraham@hotmail.com>
2024-05-10 16:25:36 -0500

    Segundo commit

--- src/app/numeros-operaciones/numeros-operaciones.component.ts
+++ src/app/numeros-operaciones/numeros-operaciones.component.ts
@@ -1,29 +1,46 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-numeros-operaciones',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './numeros-operaciones.component.html',
   styleUrl: './numeros-operaciones.component.css'
 })
 export class NumerosOperacionesComponent {
   numeros: number[] = [];
-  resultados: any[] = [];
+  numeroSeleccionado: number | null = null;
+  resultadoCuadrado: number | null = null;
+  resultadoMitad: number | null = null;
 
-  
   constructor() {
     for (let i = 15; i <= 70; i++) {
       this.numeros.push(i);
     }
+  }
 
-    this.numeros.forEach(num => {
-      this.resultados.push({
-        numero: num,
-        cuadrado: num * num,
-        mitad: num / 2
-      });
-    });
+  calcular(operacion: string): void {
+    if (!this.numeroSeleccionado) {
+      return;
+    }
+    switch (operacion) {
+      case 'cuadrado':
+        this.resultadoCuadrado = this.calcularCuadrado(this.numeroSeleccionado);
+        this.resultadoMitad = null;
+        break;
+      case 'mitad':
+        this.resultadoCuadrado = null;
+        this.resultadoMitad = this.calcularMitad(this.numeroSeleccionado);
+        break;
+    }
+  }
+  calcularCuadrado(numero: number): number {
+    return numero * numero;
+  }
+
+  calcularMitad(numero: number): number {
+    return numero / 2;
   }
 }
-
