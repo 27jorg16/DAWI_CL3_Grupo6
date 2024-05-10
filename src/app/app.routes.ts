f297ce07b466017dbc1e25ef2906b80cfa19421d
Ryuunotora <144704234+Ryuunotora@users.noreply.github.com>
2024-05-10 17:29:22 -0500

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { JubilacionComponent } from './jubilacion/jubilacion.component';
 import { CostoMatriculaComponent } from './costo-matricula/costo-matricula.component';
+import { MenorNumeroComponent } from './menor-numero/menor-numero.component';
 
 export const routes: Routes = [
     { path: 'jubilacion', component: JubilacionComponent },
-    { path: 'costo-matricula', component: CostoMatriculaComponent }
+    { path: 'costo-matricula', component: CostoMatriculaComponent },
+    { path: 'menor-numero', component: MenorNumeroComponent }
 ];
