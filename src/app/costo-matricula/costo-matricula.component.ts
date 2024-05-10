31a53c7c6fe6de0015abbf1c7a202fa7c3de0492
Ryuunotora <144704234+Ryuunotora@users.noreply.github.com>
2024-05-10 14:28:34 -0500

    Add files via upload

---
+++ src/app/costo-matricula/costo-matricula.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-costo-matricula',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './costo-matricula.component.html',
+  styleUrl: './costo-matricula.component.css'
+})
+export class CostoMatriculaComponent { 
+}
