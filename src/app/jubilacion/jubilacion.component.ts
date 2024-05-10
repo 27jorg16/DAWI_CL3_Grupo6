fcf9b802a46b722f07b89f1989622ab85b48de70
27jorg16 <jorgevaldivieso-@hotmail.com>
2024-05-10 09:27:30 -0500

    calcular juvilacion

--- src/app/jubilacion/jubilacion.component.ts
+++ src/app/jubilacion/jubilacion.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-jubilacion',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './jubilacion.component.html',
   styleUrl: './jubilacion.component.css'
 })
 export class JubilacionComponent {
-
+  jubilado = {
+    edad: 0,
+    sexo: "",
+    puedeJubilarse: ""
+  }
+  
+  verificarJubilacion() {
+    if (this.jubilado.sexo === 'Hombre') {
+      this.jubilado.puedeJubilarse = this.jubilado.edad >= 60 ? "¡Puede jubilarse!" : "Aún no puede jubilarse.";
+    } else if (this.jubilado.sexo === 'Mujer') {
+      this.jubilado.puedeJubilarse = this.jubilado.edad >= 54 ? "¡Puede jubilarse!" : "Aún no puede jubilarse.";
+    } else {
+      this.jubilado.puedeJubilarse = "No se puede determinar.";
+    }
+  }
+  
 }
