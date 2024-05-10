90e1438bb3deafd0f9c1c2e6aaef80c9d725cb02
Ryuunotora <144704234+Ryuunotora@users.noreply.github.com>
2024-05-10 17:31:11 -0500

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,8 @@
 import { Routes } from '@angular/router';
 import { JubilacionComponent } from './jubilacion/jubilacion.component';
-import { MenorNumeroComponent } from './menor-numero/menor-numero.component';
+
 
 export const routes: Routes = [
     { path: 'jubilacion', component: JubilacionComponent },
-    { path: 'menor-numero', component: MenorNumeroComponent },
-    
+  
 ];
